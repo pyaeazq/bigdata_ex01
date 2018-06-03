3bf20dca1c78391098476087d3c85ff9d98de2d1
pyaeazq <439467745@qq.com>
2018-06-03 11:31:03 +0800

    Add files via upload

---
+++ day02/day02(1).py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 08:32:41 2018
+
+@author: lenovo
+"""
+
+import time
+print('你好等一下')
+time.sleep(3)
+print('我来了')
+
+print('欢')
+time.sleep(1)
+print('迎')
+time.sleep(1)
+print('光')
+time.sleep(1)
+print('临')
+time.sleep(1)
